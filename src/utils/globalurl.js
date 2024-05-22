d4e56c67e8e5551631832c3a65316f630088cb30
ranajunaidhashim <ranajunaidhashim6@gmail.com>
2024-05-22 13:37:59 +0500

    changes

--- src/utils/globalurl.js
+++ src/utils/globalurl.js
@@ -1,3 +1,3 @@
-// export const baseUrl="http://localhost:3000/";
+export const baseUrl="http://localhost:3000/";
 
-export const baseUrl="https://alumni-server-alpha.vercel.app/";
+// export const baseUrl="https://alumni-server-alpha.vercel.app/";
